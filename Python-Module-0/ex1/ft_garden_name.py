77da2a0a9ebc9ca12b11755b71eaeebec0ac1228
Mehmet Özay <mehmetozay45@hotmail.com>
2026-04-29 16:12:23 +0300

    ft_garden_name.py dosyası eklendi

---
+++ Python-Module-0/ex1/ft_garden_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# ########################################################################### #
+#   shebang: 1                                                                #
+#                                                          :::      ::::::::  #
+#   ft_garden_name.py                                    :+:      :+:    :+:  #
+#                                                      +:+ +:+         +:+    #
+#   By: mozay <mozay@student.42kocaeli.com.tr>       +#+  +:+       +#+       #
+#                                                  +#+#+#+#+#+   +#+          #
+#   Created: 2026/02/25 01:27:21 by mozay               #+#    #+#            #
+#   Updated: 2026/04/29 16:03:25 by mozay              ###   ########.fr      #
+#                                                                             #
+# ########################################################################### #
+
+
+def ft_garden_name():
+    name = input("Enter garden name: ")
+    print(f"Garden: {name}")
+    print("Status: Growing well!")
