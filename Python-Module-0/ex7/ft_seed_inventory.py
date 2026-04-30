28cfc977d953413aafe61f6467e195808991123d
Mehmet Ozay <mozay@k2m01s04.42kocaeli.com.tr>
2026-04-30 18:06:52 +0300

    capitalize hatası düzeltildi

--- Python-Module-0/ex7/ft_seed_inventory.py
+++ Python-Module-0/ex7/ft_seed_inventory.py
@@ -12,6 +12,7 @@
 # ########################################################################### #
 
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    seed_type = seed_type.capitalize()
     if (unit == "packets"):
         print(f"{seed_type} seeds: {quantity} packets available")
     elif (unit == "grams"):
