d0c799dc0bbb69ce8d2d87ea61059d4449a09a0d
Mehmet Ozay <mozay@k2m01s04.42kocaeli.com.tr>
2026-05-01 17:40:37 +0300

    ft_plant_factory.py dosyası eklendi

---
+++ Python-Module-1/ex3/ft_plant_factory.py
@@ -0,0 +1,41 @@
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  ft_plant_factory.py                               :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: mozay <mozay@student.42kocaeli.com.tr>    +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/05/01 17:12:43 by mozay           #+#    #+#               #
+#  Updated: 2026/05/01 17:27:56 by mozay           ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+class Plant:
+    def __init__(self, name: str, height: int, age_plant: int):
+        self.name = name
+        self.height = height
+        self.age_plant = age_plant
+    
+    def show(self):
+        print(f"Created: {self.name}: {round(self.height, 1)}cm, \
+{self.age_plant} days old")
+
+     
+def main():
+    rose = Plant("Rose", 25.0, 30)
+    oak = Plant("Oak", 200.0, 365)
+    cactus = Plant("Cactus", 5.0, 90)
+    sunflower = Plant("Sunflower", 80.0, 45)
+    fern = Plant("Fern", 15.0, 120)
+    
+    print("=== Plant Factory Output ===")
+    rose.show()
+    oak.show()
+    cactus.show()
+    sunflower.show()
+    fern.show()
+
+
+if __name__ == "__main__":
+    main()
+
