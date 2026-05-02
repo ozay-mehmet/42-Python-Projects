68195c9afed9378169a59ba0306bd6e8f19897bc
Mehmet Ozay <mozay@k2m01s04.42kocaeli.com.tr>
2026-05-02 18:57:53 +0300

    değişkenler mypy kontrolüne göre düzenlendi

--- Python-Module-1/ex1/ft_garden_data.py
+++ Python-Module-1/ex1/ft_garden_data.py
@@ -12,6 +12,10 @@
 # ########################################################################### #
 
 class Plant:
+    name: str
+    height: int
+    age_plant: int
+
     def __init__(self, name: str, height: int, age: int):
         self.name = name
         self.height = height
