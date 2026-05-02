e2ce327271c3f87feb08cb3d293c8759f048f4a1
Mehmet Ozay <mozay@k2m01s04.42kocaeli.com.tr>
2026-05-02 18:59:53 +0300

    değişkenler mypy kontrolüne göre düzenlendi
    flake8 hataları düzeltildi

--- Python-Module-1/ex3/ft_plant_factory.py
+++ Python-Module-1/ex3/ft_plant_factory.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # ************************************************************************* #
 #                                                                           #
 #                                                      :::      ::::::::    #
@@ -5,29 +6,33 @@
 #                                                  +:+ +:+         +:+      #
 #  By: mozay <mozay@student.42kocaeli.com.tr>    +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
-#  Created: 2026/05/01 17:12:43 by mozay           #+#    #+#               #
-#  Updated: 2026/05/01 17:27:56 by mozay           ###   ########.fr        #
+#  Created: 2026/05/01 18:32:06 by mozay           #+#    #+#               #
+#  Updated: 2026/05/02 13:38:21 by mozay           ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 class Plant:
+    name: str
+    height: int
+    age_plant: int
+
     def __init__(self, name: str, height: int, age_plant: int):
         self.name = name
         self.height = height
         self.age_plant = age_plant
-    
+
     def show(self):
         print(f"Created: {self.name}: {round(self.height, 1)}cm, \
 {self.age_plant} days old")
 
-     
+
 def main():
     rose = Plant("Rose", 25.0, 30)
     oak = Plant("Oak", 200.0, 365)
     cactus = Plant("Cactus", 5.0, 90)
     sunflower = Plant("Sunflower", 80.0, 45)
     fern = Plant("Fern", 15.0, 120)
-    
+
     print("=== Plant Factory Output ===")
     rose.show()
     oak.show()
@@ -38,4 +43,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
