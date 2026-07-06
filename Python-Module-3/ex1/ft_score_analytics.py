52c79626f2bc11f2c583dd1548456ea9e55024ec
Mehmet Özay <mehmetozay45@hotmail.com>
2026-07-06 19:12:09 +0300

    Update ft_score_analytics.py to enhance score processing and output details

--- Python-Module-3/ex1/ft_score_analytics.py
+++ Python-Module-3/ex1/ft_score_analytics.py
@@ -7,12 +7,38 @@
 #  By: mozay <mozay@student.42kocaeli.com.tr>    +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/07/06 17:55:56 by mozay           #+#    #+#               #
-#  Updated: 2026/07/06 17:56:29 by mozay           ###   ########.fr        #
+#  Updated: 2026/07/06 19:11:28 by mozay           ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
+import sys
+
+
+def score_analytics() -> None:
+    score_list: list[int] = []
+    i = 1
+    total_arg = len(sys.argv) - 1
+    while i <= total_arg:
+        try:
+            score_list.append(int(sys.argv[i]))
+        except ValueError:
+            print(f"Invalid parameter: '{sys.argv[i]}'")
+        i += 1
+    if len(score_list) == 0:
+        print("No scores provided. Usage: \
+python3 ft_score_analytics.py <score1> <score2> ...")
+    else:
+        print(f"Scores processed: {score_list}")
+        print(f"Total score: {sum(score_list)}")
+        print(f"Average score: {sum(score_list) / len(score_list)}")
+        print(f"High score: {max(score_list)}")
+        print(f"Low score: {min(score_list)}")
+        print(f"Score range: {max(score_list) - min(score_list)}")
+
+
 def main() -> None:
     print("=== Player Score Analytics ===")
+    score_analytics()
 
 
 if __name__ == "__main__":
