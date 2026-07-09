e11ae8dfb87cf309830b2aabe68858aa3717505f
Mehmet Özay <mehmetozay45@hotmail.com>
2026-07-09 17:12:05 +0300

    Update ft_achievement_tracker.py to enhance achievement handling and refactor player data structure

--- Python-Module-3/ex3/ft_achievement_tracker.py
+++ Python-Module-3/ex3/ft_achievement_tracker.py
@@ -7,7 +7,7 @@
 #  By: mozay <mozay@student.42kocaeli.com.tr>    +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/07/07 16:16:54 by mozay           #+#    #+#               #
-#  Updated: 2026/07/08 19:52:23 by mozay           ###   ########.fr        #
+#  Updated: 2026/07/09 17:11:45 by mozay           ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
@@ -21,81 +21,76 @@ def gen_player_achievements() -> set[str]:
         "World Savior", "Master Explorer", "Crafting Genius",
         "Strategist", "Sharp Mind", "Hidden Path Finder",
     ]
-    achievement_count: int = random.randint(4, len(all_achievements))
+    achievement_count: int = random.randint(1, len(all_achievements))
     return set(random.sample(all_achievements, achievement_count))
 
 
-def get_all_achievements(players: dict[str, set[str]]) -> set[str]:
+def get_all_achievements(achievements: list[tuple[str, set[str]]]) -> set[str]:
     all_achievements: set[str] = set()
-    for player in players:
-        all_achievements = all_achievements.union(players[player])
+    for _, achieve in achievements:
+        all_achievements = all_achievements.union(achieve)
     return all_achievements
 
 
-def get_common_achievements(players: dict[str, set[str]]) -> set[str]:
+def get_common_achievements(
+        achievements: list[tuple[str, set[str]]]) -> set[str]:
     common_achievements: set[str] = set()
 
     first_player: bool = True
 
-    for player in players:
+    for _, achieve in achievements:
         if first_player:
-            common_achievements = players[player]
+            common_achievements = achieve
             first_player = False
         else:
-            common_achievements = common_achievements.intersection(
-                players[player])
+            common_achievements = common_achievements.intersection(achieve)
     return common_achievements
 
 
-def get_unique_achievements(player_name: str,
-                            players: dict[str, set[str]]) -> set[str]:
+def get_unique_achievements(achievements: list[tuple[str, set[str]]],
+                            achieve: set[str]) -> set[str]:
     other_players: set[str] = set()
 
-    for player in players:
-        if player != player_name:
-            other_players = other_players.union(players[player])
+    for _, ach in achievements:
+        if ach is not achieve:
+            other_players = other_players.union(ach)
 
-    return players[player_name].difference(other_players)
+    return achieve.difference(other_players)
 
 
-def get_missing_achievements(player_name: str, all_achievements: set[str],
-                             players: dict[str, set[str]]) -> set[str]:
-    return all_achievements.difference(players[player_name])
+def get_missing_achievements(all_achievements: set[str],
+                             achievement: set[str]) -> set[str]:
+    return all_achievements.difference(achievement)
 
 
 def main() -> None:
     print("=== Achievement Tracker System ===\n")
 
-    players: dict[str, set[str]] = {
-        "Alice": gen_player_achievements(),
-        "Bob": gen_player_achievements(),
-        "Charlie": gen_player_achievements(),
-        "Dylan": gen_player_achievements(),
-        }
+    players: list[str] = ["Alice", "Bob", "Charlie", "Dylan"]
+    achievements: list[tuple[str, set[str]]] = []
 
     for player in players:
-        print(f"Player {player}: {players[player]}")
+        gen = gen_player_achievements()
+        achievements.append((player, gen))
+        print(f"Player {player}: {gen}")
 
-    all_achievements: set[str] = get_all_achievements(players)
+    all_achievements: set[str] = get_all_achievements(achievements)
     print(f"\nAll distinct achievements: {all_achievements}")
 
-    common_achievements: set[str] = get_common_achievements(players)
+    common_achievements: set[str] = get_common_achievements(achievements)
     print(f"\nCommon achievements: {common_achievements}\n")
 
-    for player in players:
-        unique = get_unique_achievements(player, players)
+    for player, achieve in achievements:
+        unique = get_unique_achievements(achievements, achieve)
         print(f"Only {player} has: {unique}")
 
     print()
 
     game_achievements: set[str] = set(gen_player_achievements())
 
-    for player in players:
+    for player, achieve in achievements:
         missing: set[str] = get_missing_achievements(
-            player,
-            game_achievements,
-            players,
-        )
+            game_achievements, achieve)
         print(f"{player} is missing: {missing}")
 
 
