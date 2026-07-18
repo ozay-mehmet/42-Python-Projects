d69b4225d9e85f953c85189751bd92ae2e2e1ba7
Mehmet Özay <mehmetozay45@hotmail.com>
2026-07-18 14:22:13 +0300

    Remove dark_spell_record import from __init__.py and update __all__ declaration

--- Python-Module-6/alchemy/grimoire/__init__.py
+++ Python-Module-6/alchemy/grimoire/__init__.py
@@ -7,11 +7,10 @@
 #  By: mozay <mozay@student.42kocaeli.com.tr>    +#+  +:+       +#+         #
 #                                              +#+#+#+#+#+   +#+            #
 #  Created: 2026/07/17 19:49:19 by mozay           #+#    #+#               #
-#  Updated: 2026/07/18 12:03:39 by mozay           ###   ########.fr        #
+#  Updated: 2026/07/18 14:13:54 by mozay           ###   ########.fr        #
 #                                                                           #
 # ************************************************************************* #
 
 from .light_spellbook import light_spell_record
-from .dark_spellbook import dark_spell_record
 
-__all__ = ["light_spell_record", "dark_spell_record"]
+__all__ = ["light_spell_record"]
