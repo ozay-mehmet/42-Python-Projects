c84dd58ff18a22e0a64ef0c03778a55bc0aacbd8
Mehmet Özay <mehmetozay45@hotmail.com>
2026-07-28 18:55:05 +0300

    Add initial implementation of lambda_spells module with basic function stubs

---
+++ Python-Module-10/ex0/lambda_spells.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# ************************************************************************* #
+#                                                                           #
+#                                                      :::      ::::::::    #
+#  lambda_spells.py                                  :+:      :+:    :+:    #
+#                                                  +:+ +:+         +:+      #
+#  By: mozay <mozay@student.42kocaeli.com.tr>    +#+  +:+       +#+         #
+#                                              +#+#+#+#+#+   +#+            #
+#  Created: 2026/07/28 18:47:15 by mozay           #+#    #+#               #
+#  Updated: 2026/07/28 18:52:19 by mozay           ###   ########.fr        #
+#                                                                           #
+# ************************************************************************* #
+
+def spell_transformer(spells: list[str]) -> list[str]:
+    return spells
+
+
+def power_filter(mages: list[dict], min_power: int) -> list[dict]:
+    return mages
+
+
+def artifact_sorter(artifacts: list[dict]) -> list[dict]:
+    return artifacts
+
+
+def mage_stats(mages: list[dict]) -> dict:
+    return {}
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
